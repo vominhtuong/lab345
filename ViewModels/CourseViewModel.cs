e2a0197ee9fb22e204fe0489a47a4befcd90777c
vominhtuong <85872530+vominhtuong@users.noreply.github.com>
2021-07-05 15:00:56 +0700

    lab345

---
+++ ViewModels/CourseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
